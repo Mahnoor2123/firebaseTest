712d61373a0d44710aebda8be04dca54346a8249
Mahnoor2123 <mahikhan2999@gmail.com>
2023-02-21 11:20:58 +0500

    login page done

---
+++ assets/api/login.js
@@ -0,0 +1,23 @@
+function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (email == "" || password == "") {
+        window.alert('Please Enter All Fields')
+    } else {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                window.alert("login sucessfully")
+                // Signed in
+                var user = userCredential.user;
+                window.location.href="../../home.html"
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                window.alert(errorMessage)
+            });
+    }
+
+}
