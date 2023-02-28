51f25dc44cffa4ad5bd0e93cfbcc13b81c5d4446
Mahnoor2123 <mahikhan2999@gmail.com>
2023-02-28 12:14:52 +0500

    edit and delete blog crud

--- assets/api/blog-detail.js
+++ assets/api/blog-detail.js
@@ -14,7 +14,42 @@ db.collection('blogs').doc(myParam).get().then((blogData) => {
     </div>
 </div>
     `)
+
+    $('#blogTitle').val(blogData.data().blogTitle);
+    $("#blogDescription").val(blogData.data().blogDescription);
+
+
 }).catch((err) => {
     window.alert(err.message)
 });
 
+
+
+
+function editBlog() {
+    let updatedBlogTitle = $('#blogTitle').val();
+    let updatedBlogDescription = $('#blogDescription').val();
+    if (updatedBlogTitle == "" || updatedBlogDescription == "") {
+        window.alert("fill all fields")
+    } else {
+
+        db.collection('blogs').doc(myParam).update({
+            blogTitle: updatedBlogTitle,
+            blogDescription: updatedBlogDescription
+        }).then(() => {
+            window.alert('Updated Successfully');
+            window.location.reload()
+        }).catch((err) => {
+
+        });
+    }
+
+
+
+}
+function deleteBlog(){
+    db.collection('blogs').doc(myParam).delete().then(()=>{
+        window.alert('Delete Successfully');
+        window.location.href= '../../home.html'
+    })
+}
